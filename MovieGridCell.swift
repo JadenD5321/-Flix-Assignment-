f82fe0f12706309f7fe28a8d80cb5b3382c85507
JadenDodooTawiah <78453301+JadenD5321@users.noreply.github.com>
2022-10-01 03:09:49 -0400

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jaden Dodoo-Tawiah on 9/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
